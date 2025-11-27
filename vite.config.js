9f50ed0c2fe5d5a4d76eab2993a7a584212610ae
Terry Ntuli <meriusterry>
2025-11-27 20:31:45 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/social-worker-website/', // replace with your repo name
+  base: '/therapeutic-website/', // replace with your repo name
 });
